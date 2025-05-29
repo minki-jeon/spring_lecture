ddaa11f6a8cb2091e3a7363848bb6cf6b869e42f
minki-jeon <minki.jeon.study@gmail.com>
2025-05-29 17:21:34 +0900

    Model DTO/Thymeleaf출력-key호출방법 2가지(. / [])

---
+++ src/main/java/com/example/spring/dto/MyBean053.java
@@ -0,0 +1,15 @@
+package com.example.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyBean053 {
+    private Double weight;
+    private String info;
+    private Integer studentNumber;
+    private Boolean married;
+}
