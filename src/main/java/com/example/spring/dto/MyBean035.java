234b49aaaebcb7f8a0739d2878708a4356d9a687
minki-jeon <minki.jeon.study@gmail.com>
2025-05-29 10:54:01 +0900

    Lombok-Annotation (Data, Getter, Setter, ToString, ...)

---
+++ src/main/java/com/example/spring/dto/MyBean035.java
@@ -0,0 +1,10 @@
+package com.example.spring.dto;
+
+import lombok.Data;
+
+// @Data = Getter + Setter + ToString + EqualsAndHashCode + RequiredArgsConstructor
+@Data
+public class MyBean035 {
+    private String address;
+    private Double salary;
+}
