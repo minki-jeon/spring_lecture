0aa2095e295739afb2a5ecf181b260cc5e6530aa
minki-jeon <minki.jeon.study@gmail.com>
2025-05-29 17:30:08 +0900

    Model List<DTO>/Thymeleaf출력

---
+++ src/main/java/com/example/spring/dto/MyBean057.java
@@ -0,0 +1,13 @@
+package com.example.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MyBean057 {
+    private String name;
+    private String birthDate;
+    private Integer price;
+
+}
