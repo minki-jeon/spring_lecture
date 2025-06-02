ad8d7db8da1e0cfc04eb5b5f315c8db8d07b019c
minki-jeon <minki.jeon.study@gmail.com>
2025-06-02 15:39:28 +0900

    Session / HttpSession - getAttribute()/setAttribute()

---
+++ src/main/java/com/example/spring/controller/Controller11.java
@@ -0,0 +1,82 @@
+package com.example.spring.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("main11")
+public class Controller11 {
+
+    @GetMapping("sub1")
+    public String sub1() {
+
+        return "main11/sub1";
+    }
+
+    // HttpSession : session 객체의 타입
+    @PostMapping("sub1")
+    public String sub2(String[] item, HttpSession session) {
+        System.out.println("Controller11.sub2");
+        System.out.println("Arrays.toString(item) = " + Arrays.toString(item));
+
+        // getAttribute: session 에서 attribute 꺼내기
+        var map = (Map<String, Integer>) session.getAttribute("cart");
+
+        if (map == null) {
+            // setAttribute : session에 attribute넣기
+            map = new HashMap<>();
+            session.setAttribute("cart", map);
+        }
+
+        if (item != null && item.length > 0) {
+            for (String i : item) {
+                if (map.containsKey(i)) {
+                    Integer count = map.get(i);
+                    map.put(i, count + 1);
+                } else {
+                    map.put(i, 1);
+                }
+            }
+        }
+
+        for (var entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        return "main11/sub1";
+    }
+
+    @GetMapping("sub2")
+    public String sub3() {
+
+        return "main11/sub2";
+    }
+
+    @PostMapping("sub2")
+    public String sub4(HttpSession session, Model model) {
+        System.out.println("Controller11.sub4");
+
+        var number = (Integer) session.getAttribute("number");
+
+        if (number == null) {
+            number = 1;
+        } else {
+            number = number + 1;
+        }
+
+        session.setAttribute("number", number);
+        model.addAttribute("value", number);
+
+        return "main11/sub2";
+    }
+
+
+}
