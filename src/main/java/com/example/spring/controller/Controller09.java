e77fd710661a4569377d7620aa69d1d81ff73514
minki-jeon <minki.jeon.study@gmail.com>
2025-06-02 11:10:04 +0900

    Thymeleaf form/input-type/text/checkbox/label/number/password/date/datetime-local/email/file/hidden

---
+++ src/main/java/com/example/spring/controller/Controller09.java
@@ -0,0 +1,59 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main9")
+public class Controller09 {
+
+    @RequestMapping("sub1")
+    public String sub1() {
+
+
+        return "main9/sub1";
+    }
+
+    @RequestMapping("sub2")
+    public String sub2(String name, Integer age, String address) {
+
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        System.out.println("address = " + address);
+
+        return "main9/sub2";
+    }
+
+    @RequestMapping("sub3")
+    public String sub3(String email, Boolean married, String city, String team) {
+        System.out.println("email = " + email);
+        System.out.println("married = " + married);
+        System.out.println("city = " + city);
+        System.out.println("team = " + team);
+
+        return "main9/sub3";
+    }
+
+    @RequestMapping("sub4")
+    public String sub4() {
+
+        return "main9/sub4";
+    }
+
+    @RequestMapping("sub5")
+    public String sub5(String keyword, String email) {
+        System.out.println("keyword = " + keyword);
+        System.out.println("email = " + email);
+
+        return "main9/sub5";
+    }
+
+    public String sub6(String address, String age, String city) {
+        System.out.println("address = " + address);
+        System.out.println("age = " + age);
+        System.out.println("city = " + city);
+
+        return "main9/sub6";
+    }
+}
+
