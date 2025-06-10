123849a1b702b415aa7a6f0f099cb0407dbcf30b
minki-jeon <minki.jeon.study@gmail.com>
2025-06-10 14:40:26 +0900

    JDBC - Form/SELECT-Data + DELETE-Data

--- src/main/java/com/example/spring/controller/Controller16.java
+++ src/main/java/com/example/spring/controller/Controller16.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -135,4 +136,114 @@ public class Controller16 {
 
         return "redirect:/main16/sub2";
     }
+
+    @GetMapping("sub3")
+    public String form3(Integer id, Model model) throws SQLException {
+
+        if (id != null) {
+            String sql = """
+                    SELECT *
+                    FROM Customers
+                    WHERE CustomerID = ?
+                    """;
+            String url = "jdbc:mysql://localhost:3306/w3schools";
+            String username = "root";
+            String password = "1234";
+            Connection connection = DriverManager.getConnection(url, username, password);
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                // 조회 결과가 있을 때만
+                CustomerDto customerDto = new CustomerDto();
+                customerDto.setId(resultSet.getInt("customerId"));
+                customerDto.setName(resultSet.getString("customerName"));
+                customerDto.setContactName(resultSet.getString("contactName"));
+                customerDto.setAddress(resultSet.getString("address"));
+                customerDto.setCity(resultSet.getString("city"));
+                customerDto.setPostalCode(resultSet.getString("postalCode"));
+                customerDto.setCountry(resultSet.getString("country"));
+                model.addAttribute("customer", customerDto);
+            }
+        }
+
+        return "main16/sub3";
+    }
+
+    @PostMapping("sub3")
+    public String process3(Integer id, RedirectAttributes rttr) throws SQLException {
+        String sql = """
+                DELETE FROM Customers
+                WHERE CustomerID = ?
+                """;
+        String url = "jdbc:mysql://localhost:3306/w3schools";
+        String username = "root";
+        String password = "1234";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        statement.executeUpdate();
+
+        // 삭제 이후에도 입력한 값 유지
+        rttr.addAttribute("id", id);    // query string에 붙인다.
+
+        // get방식 페이지로 요청
+        return "redirect:/main16/sub3";
+    }
+
+    @GetMapping("sub4")
+    public String form4(Integer id, Model model) throws SQLException {
+        //* 데이터 조회
+        if (id != null) {
+            String sql = """
+                    SELECT * FROM Suppliers
+                    WHERE SupplierID = ?
+                    """;
+            String url = "jdbc:mysql://localhost:3306/w3schools";
+            String username = "root";
+            String password = "1234";
+            Connection connection = DriverManager.getConnection(url, username, password);
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                SuppliersDTO supplierDto = new SuppliersDTO();
+                supplierDto.setId(resultSet.getInt("supplierId"));
+                supplierDto.setName(resultSet.getString("supplierName"));
+                supplierDto.setContactName(resultSet.getString("contactName"));
+                supplierDto.setAddress(resultSet.getString("address"));
+                supplierDto.setCity(resultSet.getString("city"));
+                supplierDto.setPostalCode(resultSet.getString("postalCode"));
+                supplierDto.setCountry(resultSet.getString("country"));
+                supplierDto.setPhone(resultSet.getString("phone"));
+                model.addAttribute("supplier", supplierDto);
+            }
+
+        }
+
+        return "main16/sub4";
+    }
+
+    @PostMapping("sub4")
+    public String process4(Integer id, RedirectAttributes rttr) throws SQLException {
+        //* 데이터 삭제
+        String sql = """
+                DELETE FROM Suppliers 
+                WHERE SupplierID = ?
+                """;
+        String url = "jdbc:mysql://localhost:3306/w3schools";
+        String username = "root";
+        String password = "1234";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        statement.executeUpdate();
+
+        rttr.addAttribute("id", id);
+
+        return "redirect:/main16/sub4";
+    }
+
+
 }
