cb4c237534875b3d66595347279e0e9baee4ccef
minki-jeon <minki.jeon.study@gmail.com>
2025-06-11 12:00:33 +0900

    JDBC DataTypes - Insert/Select (String, Number, DateTime)

--- src/main/java/com/example/spring/controller/Controller17.java
+++ src/main/java/com/example/spring/controller/Controller17.java
@@ -1,11 +1,16 @@
 package com.example.spring.controller;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Controller
 @RequestMapping("main17")
@@ -35,6 +40,7 @@ public class Controller17 {
 
         return null;
     }
+
     @GetMapping("sub2")
     public String sub2(Model model) throws Exception {
         String sql = """
@@ -119,4 +125,133 @@ public class Controller17 {
         return null;
     }
 
+    @Data
+    static class Dto5 {
+        private String col1;            // VARCHAR
+        private Integer col2;           // INT
+        private Long col3;              // BIGINT
+        private Double col4;            // DEC/DECIMAL
+        private LocalDate col5;         // DATE
+        private LocalTime col6;              // TIME
+        private LocalDateTime col7;     // DATETIME
+    }
+
+    @GetMapping("sub5")
+    public String sub5(Model model) throws Exception {
+        String sql = """
+                select * FROM table12
+                LIMIT 1
+                """;
+
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "root";
+        String password = "1234";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            Dto5 dto5 = new Dto5();
+            dto5.setCol1(resultSet.getString("col1"));    // VARCHAR
+            dto5.setCol2(resultSet.getInt("col2"));    // INT
+            dto5.setCol3(resultSet.getLong("col3"));    // BIGINT
+            dto5.setCol4(resultSet.getDouble("col4"));    // DECIMAL
+            dto5.setCol5(resultSet.getDate("col5").toLocalDate());    // DATE
+            dto5.setCol6(resultSet.getTime("col6").toLocalTime());    // TIME
+            dto5.setCol7(resultSet.getTimestamp("col7").toLocalDateTime());    // DATETIME
+            model.addAttribute("dto5", dto5);
+        }
+
+        return "main17/sub5";
+    }
+
+    @GetMapping("sub6")
+    public String sub6(Model model) throws Exception {
+        return "main17/sub6";
+    }
+
+    @PostMapping("sub6")
+    public String process6(Dto5 dto5) throws Exception {
+//        System.out.println("dto5 = " + dto5);
+        String sql = """
+                INSERT INTO table12
+                (col1, col2, col3, col4, col5, col6, col7)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                """;
+
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "root";
+        String password = "1234";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, dto5.getCol1());
+        statement.setInt(2, dto5.getCol2());
+        statement.setLong(3, dto5.getCol3());
+        statement.setDouble(4, dto5.getCol4());
+        statement.setDate(5, Date.valueOf(dto5.getCol5()));
+        statement.setTime(6, Time.valueOf(dto5.getCol6()));
+        statement.setTimestamp(7, Timestamp.valueOf(dto5.getCol7()));
+
+        statement.executeUpdate();
+
+        return "redirect:/main17/sub6";
+    }
+
+    // CREATE TABLE table13(name(텍스트), birth_date(날짜), score(소숫점2), born_at(날짜시간))
+    // DTO7, Form(Html), Request Handler Method (get, post)
+    @Data
+    static class Dto7 {
+        private String name;
+        private LocalDate birthDate;
+        private Double score;
+        private LocalDateTime bornAt;
+    }
+
+    @GetMapping("sub7")
+    public String sub7(Model model) throws Exception {
+        String sql = "SELECT * FROM table13";
+        
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "root";
+        String password = "1234";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            Dto7 dto7 = new Dto7();
+            dto7.setName(resultSet.getString("name"));
+            dto7.setBirthDate(resultSet.getDate("birth_date").toLocalDate());
+            dto7.setScore(resultSet.getDouble("score"));
+            dto7.setBornAt(resultSet.getTimestamp("born_at").toLocalDateTime());
+//            model.addAttribute("dto7", dto7);
+            System.out.println("dto7 = " + dto7);
+        }
+
+        return "main17/sub7";
+    }
+
+    @PostMapping("sub7")
+    public String process7(Dto7 dto7) throws Exception {
+//        System.out.println("dto7 = " + dto7);
+
+        String sql = """
+                INSERT INTO table13
+                (name, birth_date, score, born_at)
+                VALUES (?, ?, ?, ?)
+                """;
+        
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "root";
+        String password = "1234";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, dto7.getName());
+        statement.setDate(2, Date.valueOf(dto7.getBirthDate()));
+        statement.setDouble(3, dto7.getScore());
+        statement.setTimestamp(4, Timestamp.valueOf(dto7.getBornAt()));
+        statement.executeUpdate();
+
+        return "redirect:/main17/sub7";
+    }
 }
