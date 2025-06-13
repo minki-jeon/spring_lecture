ea1f311e7aaa533981d0c9943da0cb39a4a37677
minki-jeon <minki.jeon.study@gmail.com>
2025-06-13 12:45:50 +0900

    JDBC - Transaction, rollback(), commit() 실습

--- src/main/java/com/example/spring/controller/Controller18.java
+++ src/main/java/com/example/spring/controller/Controller18.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -176,5 +177,94 @@ public class Controller18 {
 
         return "redirect:/main18/sub1";
 
+    }
+
+    @GetMapping("sub6")
+    public String sub6(Model model) throws Exception {
+        String sql = """
+                SELECT *
+                FROM table53
+                """;
+
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "root";
+        String password = "1234";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+        List<Map<String, Object>> list = new ArrayList<>();
+        while (resultSet.next()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("user", resultSet.getString("user"));
+            map.put("money", resultSet.getInt("money"));
+            list.add(map);
+        }
+
+        model.addAttribute("list", list);
+
+        return "main18/sub6";
+    }
+
+    @PostMapping("sub7")
+    public String sub7(Model model, RedirectAttributes rttr) throws Exception {
+        // b->a (500원)
+        String sql1 = """
+                UPDATE table53
+                SET money = money - 500
+                WHERE user = 'a'
+                """;
+        String sql2 = """
+                UPDATE table53
+                SET money = money + 500
+                WHERE user = 'b'
+                """;
+
+        // db
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "root";
+        String password = "1234";
+
+//        try {
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+//            try (connection) {
+                connection.setAutoCommit(false);
+                try {
+                    PreparedStatement statement = connection.prepareStatement(sql1);
+                    PreparedStatement statement1 = connection.prepareStatement(sql2);
+
+//                    try (statement1; statement) {
+                        // b의 돈 500 차감
+                        // update1 exec
+                        statement.executeUpdate();
+
+                        double random = Math.random();
+
+                        // 50% 확률로 성공or실패 -> 실패시 rollback, 성공시 commit
+                        if (random < 0.5) {
+                            // 50% 확률로 Exception
+                            throw new Exception("네트워크 오류");
+                        }
+
+                        // a의 돈 500 증가
+                        // update2 exec
+                        statement1.executeUpdate();
+//                    }
+                    connection.commit();
+//                    System.out.println("성공");
+                    rttr.addFlashAttribute("message", "송금 성공");
+
+                } catch (Exception e) {
+                    connection.rollback();
+//                    System.out.println("실패");
+                    rttr.addFlashAttribute("message", "송금 실패");
+
+                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        return "redirect:/main18/sub6";
     }
 }
