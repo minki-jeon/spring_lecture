bf12631ba47f729ab3e88bd246b2666694d9b103
minki-jeon <minki.jeon.study@gmail.com>
2025-06-17 11:41:42 +0900

    JPA - 실행 쿼리 작성 @Query(SQL/JPQL) / 매개변수 삽입

--- src/main/java/com/example/spring/repository/Entity16Repository.java
+++ src/main/java/com/example/spring/repository/Entity16Repository.java
@@ -41,4 +41,39 @@ public interface Entity16Repository extends JpaRepository<Entity16, Integer> {
             WHERE customer_name LIKE :keyword
             """, nativeQuery = true)
     List<Entity16> query4(String keyword);
+
+    // SQL : Table 대상으로 작성
+    /*
+    @Query(value = """
+            SELECT *
+            FROM jpa.customer
+            WHERE country = :country
+            """, nativeQuery = true)
+     */
+    // JPQL : Entity 대상으로 작성
+    //    @Query(value = """ """, nativeQuery = false)  // nativeQuery = false : Default
+    @Query("""
+            SELECT e
+            FROM Entity16 e
+            WHERE e.country = :country
+            """)
+    List<Entity16> query5(String country);
+
+    // 도시 이름으로 고객 조회하는 쿼리 JPQL로 작성
+    @Query("""
+            SELECT e 
+            FROM Entity16 e
+            WHERE e.city = :city
+            """)
+    List<Entity16> query6(String city);
+
+    //
+    @Query("""
+            SELECT c
+            FROM Entity16 c
+            WHERE c.customerName LIKE :keyword
+            """)
+    List<Entity16> query7(String keyword);
+
+
 }
--- src/main/java/com/example/spring/repository/Entity17Repository.java
+++ src/main/java/com/example/spring/repository/Entity17Repository.java
@@ -1,8 +1,10 @@
 package com.example.spring.repository;
 
+import com.example.spring.entity.Entity16;
 import com.example.spring.entity.Entity17;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -22,4 +24,14 @@ public interface Entity17Repository extends JpaRepository<Entity17, Integer> {
                OR first_name LIKE :keyword
             """, nativeQuery = true)
     List<Entity17> query2(String keyword);
+
+    // 직원 이름 검색
+    @Query("""
+            SELECT e
+            FROM Entity17 e
+            WHERE e.firstName LIKE :keyword
+               OR e.lastName LIKE :keyword
+            """)
+    List<Entity17> query3(String keyword);
+//    List<Entity17> query3(@Param("keyword") String keyword);  // @Param 권장되는 방법
 }
