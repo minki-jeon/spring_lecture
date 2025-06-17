e22cc46133e8829ccb4939edb37c7b23d8ea4dac
minki-jeon <minki.jeon.study@gmail.com>
2025-06-17 12:38:16 +0900

    JPA - SQL/JPQL/DerivedQueryMethod 실습

--- sql/32jpa2.sql
+++ sql/32jpa2.sql
@@ -33,3 +33,27 @@ SELECT LastName, FirstName, BirthDate, Photo, Notes
 FROM w3schools.Employees;
 SELECT * FROM jpa.employee;
 
+
+
+
+DESC w3schools.Suppliers;
+CREATE TABLE supplier(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    supplier_name VARCHAR(255),
+    contact_name VARCHAR(255),
+    address VARCHAR(255),
+    city VARCHAR(255),
+    postal_code VARCHAR(255),
+    country VARCHAR(255),
+    phone VARCHAR(255)
+);
+INSERT INTO supplier (supplier_name, contact_name, address, city, postal_code, country, phone)
+SELECT suppliername,
+       contactname,
+       address,
+       city,
+       postalcode,
+       country,
+       phone
+FROM w3schools.Suppliers;
+SELECT * FROM supplier;
