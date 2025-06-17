222ffa150a31075c3f2d8b1cf76a228862ceef9b
minki-jeon <minki.jeon.study@gmail.com>
2025-06-17 12:35:33 +0900

    JPA - 실행 쿼리 작성 @Query(SQL/JPQL),매개변수,DerivedQueryMethod

---
+++ src/main/java/com/example/spring/repository/Entity18Repository.java
@@ -0,0 +1,46 @@
+package com.example.spring.repository;
+
+import com.example.spring.entity.Entity18;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface Entity18Repository extends JpaRepository<Entity18, Integer> {
+    // * 쿼리 생성 방법
+    // findById
+    // save
+    // deleteById
+
+    // SQL
+    // JPQL
+
+    // Query Method : 메소드 이름으로 쿼리만들기
+
+    /*
+    SELECT * FROM supplier WHERE country = :country
+     */
+    List<Entity18> findByCountry(String country);
+
+    /*
+    SELECT * FROM supplier WHERE city = :city
+     */
+    List<Entity18> findByCity(String city);
+
+    /*
+    SELECT * FROM supplier WHERE supplier_name LIKE :keyword
+     */
+    // keyword = "%"+keyword+"%" (와일드카드(%) 결합 필요)
+    List<Entity18> findBySupplierNameLike(String keyword);
+
+    /*
+    SELECT * FROM supplier WHERE supplier_name LIKE :keyword
+     */
+    // keyword = keyword (와일드카드(%) 불필요)
+    List<Entity18> findBySupplierNameContains(String keyword);
+
+    /*
+    SELECT * FROM supplier WHERE country IN (?, ?, ?)
+     */
+    List<Entity18> findByCountryIn(List<String> country);
+
+}
