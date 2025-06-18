4a6fb0019c94f005ef6dd6fb3e8a9765606fa079
minki-jeon <minki.jeon.study@gmail.com>
2025-06-18 12:03:35 +0900

    JPA - QueryMethod / 트랜잭션

--- sql/33jpa3.sql
+++ sql/33jpa3.sql
@@ -5,3 +5,19 @@ SELECT COUNT(*) FROM customer;
 INSERT INTO customer (customer_name, contact_name, address, city, postal_code, country)
 SELECT customer_name, contact_name, address, city, postal_code, country FROM customer;
 
+
+select count(*) from customer where country = 'mexico';
+select count(*) from customer where id = 1;
+select * from customer ;
+
+
+CREATE TABLE my_table20
+(
+    name  VARCHAR(30),
+    money INT,
+    PRIMARY KEY (name)
+);
+INSERT INTO my_table20(name, money)
+VALUES ('a', 10000),
+       ('b', 10000);
+
