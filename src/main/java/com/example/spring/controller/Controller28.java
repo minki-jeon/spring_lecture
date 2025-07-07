55cfeba0a09a9ddccf900ce0be9f20a56f59b558
minki-jeon <minki.jeon.study@gmail.com>
2025-07-07 10:40:27 +0900

    axios react Response - queryString(params)/URLSearchParams()

--- src/main/java/com/example/spring/controller/Controller28.java
+++ src/main/java/com/example/spring/controller/Controller28.java
@@ -88,4 +88,22 @@ public class Controller28 {
         System.out.println();
         return null;
     }
+
+    @GetMapping("sub13")
+    public String sub13(String name, Integer age, String address) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        System.out.println("address = " + address);
+        System.out.println();
+        return null;
+    }
+
+    @GetMapping("sub14")
+    public String sub14(String city, String country, Integer id) {
+        System.out.println("city = " + city);
+        System.out.println("country = " + country);
+        System.out.println("id = " + id);
+        System.out.println();
+        return null;
+    }
 }
