469e3a70885117c322ce38c305fed7acabd901ed
minki-jeon <minki.jeon.study@gmail.com>
2025-07-07 14:43:40 +0900

    axios react Response - queryString(params)/URLSearchParams()/useStat/json

--- src/main/java/com/example/spring/controller/Controller29.java
+++ src/main/java/com/example/spring/controller/Controller29.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -14,27 +15,28 @@ import java.util.Map;
 public class Controller29 {
 
     @PostMapping("sub1")
-    public String sub1(){
+    public String sub1() {
         System.out.println("Controller29.sub1");
 
         return null;
     }
+
     @PostMapping("sub2")
-    public String sub2(){
+    public String sub2() {
         System.out.println("Controller29.sub2");
 
         return null;
     }
 
     @PostMapping("sub3")
-    public String sub3(){
+    public String sub3() {
         System.out.println("Controller29.sub3");
 
         return null;
     }
 
     @PostMapping("sub4")
-    public String sub4(@RequestBody Map<String, Object> map){
+    public String sub4(@RequestBody Map<String, Object> map) {
         // @RequestBody : JSON을 해당 타입으로 역직렬화(Decoding, Parsing)
         System.out.println("Controller29.sub4");
         System.out.println("map = " + map);
@@ -43,7 +45,7 @@ public class Controller29 {
     }
 
     @PostMapping("sub5")
-    public String sub5(@RequestBody Map<String, Object> map){
+    public String sub5(@RequestBody Map<String, Object> map) {
         System.out.println("Controller29.sub5");
         System.out.println("map = " + map); // {address=seoul, score=88.12, country=korea}
 
@@ -51,7 +53,7 @@ public class Controller29 {
     }
 
     @PostMapping("sub6")
-    public String sub6(@RequestBody Map<String, Object> map){
+    public String sub6(@RequestBody Map<String, Object> map) {
         Object val1 = map.get("val1");
         Object val2 = map.get("val2");
         Object val3 = map.get("val3");
@@ -68,4 +70,75 @@ public class Controller29 {
 
         return null;
     }
+
+    @PostMapping("sub7")
+    public String sub7(@RequestBody Map<String, Object> map) {
+        Object address = map.get("address");
+        Object score = map.get("score");
+        Object age = map.get("age");
+        Object married = map.get("married");
+
+        System.out.println(address.getClass().getSimpleName());
+        System.out.println(score.getClass().getSimpleName());
+        System.out.println(age.getClass().getSimpleName());
+        System.out.println(married.getClass().getSimpleName());
+
+        System.out.println(map);
+
+        return null;
+    }
+
+    @PostMapping("sub8")
+    public String sub8(@RequestBody Map<String, Object> map) {
+        Object address = map.get("address");
+        Object age = map.get("age");
+        Object fruits = map.get("fruits");
+
+        // JSON의 array 타입은 JAVA의 ArrayList로 파싱된다.
+        System.out.println(fruits.getClass().getSimpleName());
+        System.out.println("fruits = " + fruits);
+
+        return null;
+    }
+
+    @PostMapping("sub9")
+    public String sub9(@RequestBody Map<String, Object> map) {
+        Object name = map.get("name");
+        Object teams = map.get("teams");        // ArrayList
+        Object person = map.get("person");      // Object -> Map 으로 parsing
+
+        System.out.println(person.getClass().getSimpleName());
+        System.out.println(person);
+
+        return null;
+    }
+
+    @PostMapping("sub10")
+    public String sub10(@RequestBody Map<String, Object> map) {
+        Object cityList = map.get("cityList");
+        Object team = map.get("team");
+
+        System.out.println(cityList.getClass().getSimpleName());    // ArrayList
+        System.out.println(team.getClass().getSimpleName());        // ...Map
+
+        System.out.println(cityList);
+        System.out.println(team);
+
+        return null;
+    }
+
+    @PostMapping("sub11")
+    public String sub11(@RequestBody List<Object> data) {
+        System.out.println(data);
+
+        return null;
+    }
+
+    @PostMapping("sub12")
+    public String sub12(@RequestBody List<Object> data) {
+        System.out.println(data);
+
+        return null;
+    }
+
 }
